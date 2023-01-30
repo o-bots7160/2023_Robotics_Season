90cb9f3b3c166984fc5e340839dadb0c123ab95a
LudingtonRobotics <31971253+CyberGaming424@users.noreply.github.com>
2023-01-30 18:19:54 -0500

    Update ManipulatorControl.java

--- src/main/java/frc/robot/ManipulatorControl.java
+++ src/main/java/frc/robot/ManipulatorControl.java
@@ -38,7 +38,20 @@ public class ManipulatorControl {
 
    private void liftInit(){
       lift = new CANSparkMax(50, MotorType.kBrushless);
+      kP_Lift         = 0;
+      kI_Lift         = 0;
+      kD_Lift         = 0;
+      kIz_Lift        = 0;
+      kFF_Lift        = 0;
+      kMaxOutput_Lift = 0;
+      kMinOutput_Lift = 0;
       pid_Lift = lift.getPIDController();
+      pid_Lift.setP(kP_Lift);
+      pid_Lift.setI(kI_Lift);
+      pid_Lift.setD(kD_Lift);
+      pid_Lift.setIZone(kIz_Lift);
+      pid_Lift.setFF(kFF_Lift);
+      pid_Lift.setOutputRange(kMinOutput_Lift, kMaxOutput_Lift);
       en_Lift = lift.getEncoder();
    }
 
@@ -48,13 +61,38 @@ public class ManipulatorControl {
 
    private void wristInit(){
       wrist = new CANSparkMax(52, MotorType.kBrushless);
+      kP_Wrist         = 0;
+      kI_Wrist         = 0;
+      kD_Wrist         = 0;
+      kIz_Wrist        = 0;
+      kFF_Wrist        = 0;
+      kMaxOutput_Wrist = 0;
+      kMinOutput_Wrist = 0;
       pid_Wrist = wrist.getPIDController();
+      pid_Wrist.setP(kP_Wrist);
+      pid_Wrist.setI(kI_Wrist);
+      pid_Wrist.setD(kD_Wrist);
+      pid_Wrist.setIZone(kIz_Wrist);
+      pid_Wrist.setFF(kFF_Wrist);
+      pid_Wrist.setOutputRange(kMinOutput_Wrist, kMaxOutput_Wrist);
       en_Wrist = wrist.getEncoder();
    }
 
    private void clawInit(){
       claw = new CANSparkMax(53, MotorType.kBrushless);
+      kP_Claw         = 0;
+      kI_Claw         = 0;
+      kD_Claw         = 0;
+      kIz_Claw        = 0;
+      kFF_Claw        = 0;
+      kMaxOutput_Claw = 0;
+      kMinOutput_Claw = 0;
       pid_Claw = claw.getPIDController();
+      pid_Claw.setP(kP_Claw);
+      pid_Claw.setI(kI_Claw);
+      pid_Claw.setD(kD_Claw);
+      pid_Claw.setIZone(kIz_Claw);
+      pid_Claw.setFF(kFF_Claw);
       en_Claw = claw.getEncoder();
    }
    
